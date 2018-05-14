1482820cfeaf40412431b13c9b54cb762b9554be
Conor Hamilton <conhamil19@gmail.com>
2018-05-14 11:11:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 def line(deli) 
   if deli.length == 0 
     puts "The line is currently empty."
-  else
+  elsif deli > 0
     new_list = []
     counter = 0
     deli.each do |name|
     counter += 1 
     new_list.push("#{counter}. #{name}")
-    puts "The line is currently: #{new_list.join(" ")}"
   end
+     puts "The line is currently: #{new_list.join(" ")}"
 end
 end
