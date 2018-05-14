681948e57021d1d770d46a1f8c429a4cf210c8b3
Conor Hamilton <conhamil19@gmail.com>
2018-05-14 10:54:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 def line(deli) 
   if deli.length == 0 
     puts "The line is currently empty."
+  else
+    counter = 0
+    deli.each do |name|
+    counter += 1 
+    puts "The line is currently: #{counter}. #{name} "
   end
 end
